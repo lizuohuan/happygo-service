7b6f4de12de65f55b144736fa9a9a9b2008d8dfe
kalen <kalen>
2018-02-02 21:30:56 +0800

    .

--- src/main/java/com/magicbeans/happygo/service/IBannerService.java
+++ src/main/java/com/magicbeans/happygo/service/IBannerService.java
@@ -1,11 +1,16 @@
 package com.magicbeans.happygo.service;
 
+import com.magicbeans.base.Pages;
 import com.magicbeans.happygo.entity.Banner;
 import com.magicbeans.base.BaseService;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
- *  服务类
+ *  banner
  * </p>
  *
  * @author null123
@@ -13,6 +18,12 @@ import com.magicbeans.base.BaseService;
  */
 public interface IBannerService extends BaseService<Banner> {
 
+    /**
+     * banner列表
+     * @param map
+     * @return
+     */
+    Pages<Banner> list(Map<String,Object> map, Pages pages);
 
 
 }
--- src/main/java/com/magicbeans/happygo/service/impl/BannerServiceImpl.java
+++ src/main/java/com/magicbeans/happygo/service/impl/BannerServiceImpl.java
@@ -1,10 +1,16 @@
 package com.magicbeans.happygo.service.impl;
 
+import com.magicbeans.base.Pages;
 import com.magicbeans.happygo.entity.Banner;
 import com.magicbeans.happygo.mapper.BannerMapper;
 import com.magicbeans.happygo.service.IBannerService;
 import com.magicbeans.base.BaseServiceImp;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -17,4 +23,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class BannerServiceImpl extends BaseServiceImp<BannerMapper, Banner> implements IBannerService {
 
+    @Resource
+    private BannerMapper bannerMapper;
+
+    @Override
+    public Pages<Banner> list(Map<String, Object> map, Pages pages) {
+
+        int count = bannerMapper.listCount(map);
+        pages.setTotal(count);
+        if (count > 0) {
+            map.put("pages",pages);
+            pages.setRecords(bannerMapper.list(map));
+        }
+
+        return pages;
+    }
 }
